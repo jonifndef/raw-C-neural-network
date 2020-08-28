4c6dfc24e7c3038c3770681d5f8937466187d371
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2020-08-28 23:41:47 +0200

    Start implementing dynamic matrix

--- src/utils/matrix.h
+++ src/utils/matrix.h
@@ -8,6 +8,16 @@ typedef struct MatrixDoubles
     double **data;
 } MatrixDoubles;
 
+// TODO: use dynamic array as data member?
+typedef struct DynamicMatrix
+{
+    int rows;
+    int columns;
+    int rowCapacity;
+    int columnCapacity;
+    double **data;
+} DynamicMatrix;
+
 // Matrix functions
 MatrixDoubles* createMatrixDoubles(int numRows, int numColumns);
 void freeMatrixDoublesContents(MatrixDoubles *matrix);
@@ -16,5 +26,21 @@ void setMatrixDoubelsColumn(MatrixDoubles *matrix, int column, double *data);
 double* getMatrixDoublesRow(const MatrixDoubles *matrix, int row);
 //double* getMatrixDoublesColumn(MatrixDoubles *matrix, int column);
 double getMatrixDoublesElement(const MatrixDoubles *matrix, int row, int column);
+
+// Dynamic matrix functions - exclusively for doubles
+DynamicMatrix* createDynamicMatrix();
+void pushRow(DynamicMatrix *matrix, double *row);
+void pushColumn(DynamicMatrix *matrix, double *row);
+void pushRowElement(DynamicMatrix *matrix, int row, double element);
+void pushColumnElement(DynamicMatrix *matrix, int column, double element);
+
+double* getDynamicMatrixRow(DynamicMatrix *matrix, int row);
+double* getDynamicMatrixColumn(DynamicMatrix *matrix, int column);
+double getDynamicMatrixElement(DynamicMatrix *matrix, int row, int column);
+
+void transposeDynamicMatrix(DynamicMatrix *matrix);
+
+void clearDynamicMatrix(DynamicMatrix *matrix);
+void freeDynamicMatrix(DynamicMatrix *matrix);
 
 #endif //MATRIXDOUBLES
