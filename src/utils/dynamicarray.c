09d8c43601c15349f88d18b61b85a7dee1e65797
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2020-12-14 20:42:12 +0100

    Add dyn arr element access/modifier functions

--- src/utils/dynamicarray.c
+++ src/utils/dynamicarray.c
@@ -4,24 +4,24 @@
 
 #include "dynamicarray.h"
 
-DynamicArray* createDynamicArr()
+static bool insertAndMoveDynamicArr(DynamicArray *array,
+                                    int position,
+                                    double element)
 {
-    DynamicArray *array = calloc(1, sizeof(DynamicArray));
-    if (array == NULL)
+    DynamicArray *arrCopy = createDynamicArr();
+    if (!copyDynamicArr(arrCopy, array))
     {
-        return NULL;
+        return false;
     }
 
-    // default capacity 4, seems reasonable, right?
-    array->capacity = 4;
-    array->size = 0;
-    array->data = calloc(array->capacity, sizeof(double));
-    if (array->data == NULL)
+    for (int i = position; i < array->size; i++)
     {
-        return NULL;
+        array->data[i + 1] = arrCopy->data[i];
     }
+    array->data[position] = element;
+    array->size++;
 
-    return array;
+    return true;
 }
 
 bool reAllocDynamicArr(DynamicArray *array, int newCapacity)
@@ -40,6 +40,26 @@ bool reAllocDynamicArr(DynamicArray *array, int newCapacity)
 
     array->data = newData;
     return true;
+}
+
+DynamicArray* createDynamicArr()
+{
+    DynamicArray *array = calloc(1, sizeof(DynamicArray));
+    if (array == NULL)
+    {
+        return NULL;
+    }
+
+    // default capacity 4, seems reasonable, right?
+    array->capacity = 4;
+    array->size = 0;
+    array->data = calloc(array->capacity, sizeof(double));
+    if (array->data == NULL)
+    {
+        return NULL;
+    }
+
+    return array;
 }
 
 void freeDynamicArr(DynamicArray *array)
@@ -61,6 +81,44 @@ bool pushBackDynamicArr(DynamicArray *array, double element)
 
     array->data[array->size++] = element;
     return true;
+}
+
+bool insert(DynamicArray *array, int position, double element)
+{
+    if (position < array->size && position >= 0)
+    {
+        if (position == array->size - 1)
+        {
+            if (!pushBackDynamicArr(array, element))
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if (array->size >= array->capacity)
+            {
+                if (!reAllocDynamicArr(array, array->capacity + (array->capacity / 2)))
+                {
+                    return false;
+                }
+            }
+            if (!insertAndMoveDynamicArr(array, position, element))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+    return false;
+}
+
+void popDynamicArr(DynamicArray *array)
+{
+    if (array->size > 0)
+    {
+        array->size--;
+    }
 }
 
 bool setDynamicArrElement(DynamicArray *array, int index, double value)
@@ -97,6 +155,16 @@ double getDynamicArrElement(const DynamicArray *array, int index)
 {
     //return (index <= array->capacity) ? array->data[index] : 0.0;
     return (index <= array->capacity) ? array->data[index] : -999;
+}
+
+double backDynamicArr(const DynamicArray *array)
+{
+    return array->data[array->size - 1];
+}
+
+double frontDynamicArr(const DynamicArray *array)
+{
+    return array->data[0];
 }
 
 bool copyDynamicArr(DynamicArray *destination, const DynamicArray *source)
--- src/utils/dynamicarray.h
+++ src/utils/dynamicarray.h
@@ -10,14 +10,23 @@ typedef struct DynamicArray
     double *data;
 } DynamicArray;
 
-DynamicArray* createDynamicArr();
 bool reAllocDynamicArr(DynamicArray *arr, int newCapacity);
+
+DynamicArray* createDynamicArr();
 void freeDynamicArr(DynamicArray *array);
+
 bool pushBackDynamicArr(DynamicArray *array, double element);
+bool insert(DynamicArray *array, int position, double element);
+void popDynamicArr(DynamicArray *array);
+
 bool setDynamicArrElement(DynamicArray *array, int index, double value);
 bool setDynamicArrRow(DynamicArray *array, const double *row, int size);
+
 int getDynamicArrSize(const DynamicArray *array);
 double getDynamicArrElement(const DynamicArray *array, int index);
+double backDynamicArr(const DynamicArray *array);
+double frontDynamicArr(const DynamicArray *array);
+
 bool copyDynamicArr(DynamicArray *destination, const DynamicArray *source);
 void clearDynamicArr(DynamicArray *array);
 
