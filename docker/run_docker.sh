0b2c339d3a9c7b1d33b329c32b6113afb60080ec
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2023-01-27 13:19:33 +0100

    Tweak run_docker.sh script

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,5 +1,8 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 docker run -it --rm \
-    --user 1000:1000 \
-    --volume $(pwd):/workspace \
+    --user user \
+    --volume ${SCRIPT_DIR}/../:/home/user/raw-C-neural-network \
+    --workdir /home/user/raw-C-neural-network \
     raw-c-neural-network-image:0.0.1 \
     bash
