af84419c870e5046599c14439ecb79bce823f70e
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2024-02-26 22:39:07 +0100

    Add the other test cases back

--- tests/runNeuralNetworkTest.c
+++ tests/runNeuralNetworkTest.c
@@ -9,17 +9,17 @@ int main(int argc, char **argv)
     SRunner *sr = srunner_create(s1);
     int numFailed;
 
-    //TCase *tc1_1 = tcase_create("batches_and_layers_test");
-    //suite_add_tcase(s1, tc1_1);
-    //tcase_add_test(tc1_1, batches_and_layers_test);
+    TCase *tc1_1 = tcase_create("batches_and_layers_test");
+    suite_add_tcase(s1, tc1_1);
+    tcase_add_test(tc1_1, batches_and_layers_test);
 
-    //TCase *tc1_2 = tcase_create("activation_functions_test");
-    //suite_add_tcase(s1, tc1_2);
-    //tcase_add_test(tc1_2, activation_functions_test);
+    TCase *tc1_2 = tcase_create("activation_functions_test");
+    suite_add_tcase(s1, tc1_2);
+    tcase_add_test(tc1_2, activation_functions_test);
 
-    //TCase *tc1_3 = tcase_create("loss_test");
-    //suite_add_tcase(s1, tc1_3);
-    //tcase_add_test(tc1_3, loss_test);
+    TCase *tc1_3 = tcase_create("loss_test");
+    suite_add_tcase(s1, tc1_3);
+    tcase_add_test(tc1_3, loss_test);
 
     TCase *tc1_4 = tcase_create("loss_with_spiral_data_test");
     suite_add_tcase(s1, tc1_4);
